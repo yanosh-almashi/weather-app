6c3b194ffdbb8922042fbce81c06680c791af333
yanosh-almashi <yanosh.almashi@gmail.com>
2019-09-23 18:47:39 +0300

    add get city API call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,15 @@
 const key = 'aXNcrTLg7uaGow6AA1UYLglGipukJRNy';
+
+const getCity = async city => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity('kyiv')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
