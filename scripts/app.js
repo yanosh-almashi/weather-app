7f544f0b71bbf018e30f6c311838e34dea02ac8b
yanosh-almashi <yanosh.almashi@gmail.com>
2019-09-23 19:12:50 +0300

    add functionality for updating the location

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+const cityForm = document.querySelector('form');
+
+const updateCity = async city => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+  return {
+    cityDets: cityDets,
+    weather: weather
+  };
+};
+
+cityForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update the ui with new city
+  updateCity(city)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+});
