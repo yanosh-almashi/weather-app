535efe9b0262a818e0733260b0c6997a46e1c063
yanosh-almashi <yanosh.almashi@gmail.com>
2019-09-23 19:25:09 +0300

    update the UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,33 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = data => {
+  const { cityDets, weather } = data;
+
+  // update details template
+  details.innerHTML = `
+      <h5 class="my-3">${cityDets.EnglishName}</h5>
+      <div class="my-3">${weather.WeatherText}</div>
+      <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+      </div>
+    `;
+
+  // remove the d-none class if present
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
 
 const updateCity = async city => {
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
+
   return {
-    cityDets: cityDets,
-    weather: weather
+    cityDets,
+    weather
   };
 };
 
@@ -18,6 +40,6 @@ cityForm.addEventListener('submit', e => {
 
   // update the ui with new city
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
